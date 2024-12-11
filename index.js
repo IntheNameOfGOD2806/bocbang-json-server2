4a2553e8b781caee3fc6bf6ba80755bbf6e592f5
dattran2806 <tranthanhdat4321@gmail.com>
2024-12-11 11:50:52 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import jsonServer from 'json-server';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// ES module equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
